dd0e026cc5156571072ee01e73494fe85e34c6f0
b127exe <bilalimran6217523@gmail.com>
2023-01-21 14:55:44 +0500

    complete

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:710317574956:web:5bbc276ee89e5278dc3a89";
+  static String apiKey = "AIzaSyAWgfJST-wznb1WP7zqCCyiytex7yOqhEM";
+  static String messagingSenderId = "710317574956";
+  static String projectId = "fir-chatapp-9798e";
+  final primaryColor = const Color(0xFFee7b64);
+}
